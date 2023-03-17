0bf8a47e1a6102b3184006cbaa4b18bcff74ebc0
Nicolas Bota <Nicolas.Bota@HS-Augsburg.DE>
2023-03-17 18:44:58 +0100

    header logo and texts got router-links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="header">
-    <v-img 
-    class="header_logo"
-    v-bind:src='require("../assets/logo_oekocap_weiss.png")'
-    alt="oekoCap_logo"
-    ></v-img>
+    <router-link
+    :to='{name: "StartView"}'
+    class="header_logo">
+      <v-img
+      v-bind:src='require("../assets/logo_oekocap_weiss.png")'
+      alt="oekoCap_logo"
+      ></v-img>
+    </router-link>
     <p class="header_textcontainer">
-      <a class="header_text">Calculator</a>
-      <a class="header_text">Guideline</a>
-      <a class="header_text">Project</a>
+      <router-link
+      :to='{name: "StartView"}'
+      class="header_text">
+        <p >Calculator</p>
+      </router-link>
+      <router-link
+      :to='{name: "StartView"}'
+      class="header_text">
+        <p class="header_text">Guideline</p>
+      </router-link>
+      <router-link
+      :to='{name: "StartView"}'
+      class="header_text">
+        <p class="header_text">Project</p>
+      </router-link>
     </p>
   </div>
 </template>
