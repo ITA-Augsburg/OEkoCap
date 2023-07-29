acf2523c44eac669e50e72290399b232321a84dd
nicolasb-github <bota.nicolas@gmail.com>
2023-07-29 20:32:19 +0200

    barchart functionality implemented

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -19,32 +19,52 @@ import Chart from "chart.js/auto"
         variant="solo"
         :bg-color=color_green
         v-model=this.selected_process
-        v-on:update:model-value=[updateBarChart()]
+        v-on:update:model-value=[handleBenchmarkSelect()]
         ></v-select>
-
-        <canvas id="bar_chart" style="max-width: 420px;padding-left:30px;"></canvas>
-
-        <canvas id="pie_chart"></canvas>
 
         <div class="results_buttoncontainer">
             <div class="results_gwp_buttoncontainer">
                 <v-btn
-                :color=color_lightgrey
-                elevation="5"
-                width="225px"
-                height="55px"
-                ><p class="results_gwp_button_text">GWP</p></v-btn>
-            </div>
-
-            <div class="results_cost_buttoncontainer">
-                <v-btn
+                v-if=this.gwp_button_active
+                @click=handleGwpButton()
                 :color=color_green
                 elevation="5"
                 width="225px"
                 height="55px"
-                ><p class="results_cost_button_text">COST</p></v-btn>
+                ><p class="results_button_text active_button_text">GWP</p></v-btn>
+                <v-btn
+                v-if=!this.gwp_button_active
+                @click=handleGwpButton()
+                :color=color_lightgrey
+                elevation="5"
+                width="225px"
+                height="55px"
+                ><p class="results_button_text inactive_button_text">GWP</p></v-btn>
+            </div>
+
+            <div class="results_cost_buttoncontainer">
+                <v-btn
+                v-if=this.cost_button_active
+                @click=handleCostButton()
+                :color=color_green
+                elevation="5"
+                width="225px"
+                height="55px"
+                ><p class="results_button_text active_button_text">COST</p></v-btn>
+                <v-btn
+                v-if=!this.cost_button_active
+                @click=handleCostButton()
+                :color=color_lightgrey
+                elevation="5"
+                width="225px"
+                height="55px"
+                ><p class="results_button_text inactive_button_text">COST</p></v-btn>
             </div>
         </div>
+
+        <canvas id="bar_chart" style="max-width: 420px;padding-left:30px;"></canvas>
+
+        <canvas id="pie_chart"></canvas>
 
     </div>
 
@@ -57,32 +77,20 @@ import Chart from "chart.js/auto"
         mounted() {
             // console.log(JSON.stringify(this.test_output, null, 2))
 
-            this.checkOutput(),
-            this.setup(),
-            this.updateBarChart(),
-            new Chart("bar_chart", {
-                type: "bar",
-                data: {
-                    labels: [this.leftBarLabel, this.rightBarLabel],
-                    datasets: [{
-                        data: [[this.leftBarMin, this.leftBarMax], [this.rightBarMin, this.rightBarMax]],
-                        backgroundColor: [
-                            "red",
-                            "blue"
-                        ],
-                        borderWidth: 1,
-                        borderColor: "#777",
-                        hoverBorderWidth: 3,
-                        hoverBorderColor: "#000"
-                    }]
-                },
-                options: {
-                    title: {
-                        display: true,
-                        text: "Test Title",
-                    }
-                }
-            })
+            //check output validity
+            if(!(Object.prototype.hasOwnProperty.call(this.test_output, "processes"))) {
+                    this.$emit("setErrorMessage", "Invalid Input. No output could be generated based from the given input.")
+                    router.push({name: "ErrorView"})
+            }
+
+            //fill benchmark-select element
+            this.process_options = []
+            for(let i=0; i<this.test_benchmarks.length; i++) {
+                this.process_options[i] = this.test_benchmarks[i].name
+            }
+
+            //create initial barchart
+            this.updateBarChart()
             new Chart("pie_chart", {
                 type: "pie",
                 data: {
@@ -118,10 +126,10 @@ import Chart from "chart.js/auto"
             return {
                 process_options: [],
                 selected_process: undefined,
-                leftBarMin: 2.5,//placeholders
-                leftBarMax: 5,
-                rightBarMin: 2,
-                rightBarMax: 7,
+                gwp_button_active: false,
+                cost_button_active: false,
+                barChart: undefined,
+                barChartData: [[undefined, undefined], [undefined, undefined]],
                 leftBarLabel: "Result",
                 rightBarLabel: "Benchmark",
 
@@ -256,46 +264,102 @@ import Chart from "chart.js/auto"
                 test_benchmarks: [
                     {
                         "name": "process1",
-                        "gwp_min": 5,
-                        "gwp_max": 10,
-                        "cost_min": 15,
-                        "cost_max": 20
+                        "gwp_min": 500,
+                        "gwp_max": 1000,
+                        "cost_min": 1500,
+                        "cost_max": 2000
                     },
                     {
                         "name": "process2",
-                        "gwp_min": 5,
-                        "gwp_max": 10,
-                        "cost_min": 15,
-                        "cost_max": 20
+                        "gwp_min": 600,
+                        "gwp_max": 1100,
+                        "cost_min": 1600,
+                        "cost_max": 2100
                     },
                     {
                             "name": "process3",
-                            "gwp_min": 5,
-                            "gwp_max": 10,
-                            "cost_min": 15,
-                            "cost_max": 20
+                            "gwp_min": 700,
+                            "gwp_max": 1200,
+                            "cost_min": 1700,
+                            "cost_max": 2200
                         },
                 ],
             }
         },
         methods: {
-            checkOutput() {
-                if(!(Object.prototype.hasOwnProperty.call(this.test_output, "processes"))) {
-                    this.$emit("setErrorMessage", "Invalid Input. No output could be generated based from the given input.")
-                    router.push({name: "ErrorView"})
-                }
+            handleGwpButton() {
+                this.gwp_button_active = true
+                this.cost_button_active = false
+                this.handleButton("gwp")
             },
-            setup() {
-                this.process_options = []
-                    for(let i=0; i<this.test_benchmarks.length; i++) {
-                        this.process_options[i] = this.test_benchmarks[i].name
+            handleCostButton() {
+                this.gwp_button_active = false
+                this.cost_button_active = true
+                this.handleButton("cost")
+            },
+            handleButton(id) {
+
+                //depending on which button is "active", show gwp or cost of output
+                switch(id) {
+                    case "gwp":
+                        this.leftBarLabel = "calculated gwp"
+                        this.barChartData[0][0] = Math.round(this.test_output.gwp.minValue * 100)/100
+                        this.barChartData[0][1] = Math.round(this.test_output.gwp.maxValue * 100)/100
+                        break
+                    case "cost":
+                        this.leftBarLabel = "calculated cost"
+                        this.barChartData[0][0] = Math.round(this.test_output.cost.minValue_eur_per_kg * 100)/100
+                        this.barChartData[0][1] = Math.round(this.test_output.cost.maxValue_eur_per_kg * 100)/100
+                        break
+                }
+
+                //benchmark needs to change from gwp to cost (and vice versa) if output switches
+                this.handleBenchmarkSelect()
+                // this.updateBarChart() happens in this.handleBenchmarkSelect() alredy
+            },
+            handleBenchmarkSelect() {
+                this.test_benchmarks.forEach(element => {
+                    if(this.selected_process === element.name) {
+                        //depending on which button is "active", show gwp or cost of benchmark
+                        if(this.gwp_button_active) {
+                            this.rightBarLabel = element.name + " Benchmark gwp"
+                            this.barChartData[1][0] = Math.round(element.gwp_min * 100)/100
+                            this.barChartData[1][1] = Math.round(element.gwp_max * 100)/100
+                        } else {
+                            this.rightBarLabel = element.name + " Benchmark cost"
+                            this.barChartData[1][0] = Math.round(element.cost_min * 100)/100
+                            this.barChartData[1][1] = Math.round(element.cost_max * 100)/100
+                        }
                     }
+                })
+                this.updateBarChart()
             },
             updateBarChart() {
-                if(this.selected_process!==undefined) {
-                    //TODO
-                    return
-                }
+                if(this.barChart !== undefined) this.barChart.destroy()
+                this.barChart = new Chart("bar_chart", {
+                    type: "bar",
+                    data: {
+                        labels: [this.leftBarLabel, this.rightBarLabel],
+                        datasets: [{
+                            data: this.barChartData,
+                            backgroundColor: [
+                                "red",
+                                "blue"
+                            ],
+                            borderWidth: 1,
+                            borderColor: "#777",
+                            hoverBorderWidth: 3,
+                            hoverBorderColor: "#000"
+                        }]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: "Test Title",
+                        },
+                        animation: false
+                    }
+                })
             }
         }
     }
