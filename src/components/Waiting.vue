268af6a4c95b04a06a8494fba69198088e2115e0
nicolasb-github <bota.nicolas@gmail.com>
2023-10-18 17:15:37 +0200

    updated loading animation

--- src/components/Waiting.vue
+++ src/components/Waiting.vue
@@ -4,8 +4,12 @@
 
         <div class="loading_logo_entry_point">
             <img :src="require('../assets/loading_cogs.png')" class="loading_img loading_cogs" />
-            <img :src="require('../assets/loading_trunk.png')" class="loading_img loading_trunk" />
-            <img :src="require('../assets/loading_leaves.png')" class="loading_img loading_leaves" />
+            <img :src="require('../assets/loading_trunk_left.png')" class="loading_img loading_trunk_left" />
+            <img :src="require('../assets/loading_trunk_right.png')" class="loading_img loading_trunk_right" />
+            <img :src="require('../assets/loading_trunk_middle.png')" class="loading_img loading_trunk_middle" />
+            <img :src="require('../assets/loading_leaves_left.png')" class="loading_img loading_leaves_left" />
+            <img :src="require('../assets/loading_leaves_right.png')" class="loading_img loading_leaves_right" />
+            <img :src="require('../assets/loading_leaves_middle.png')" class="loading_img loading_leaves_middle" />
         </div>
 
     <div class="text waiting_text">Your request is being processed...<br>Please wait.</div>
